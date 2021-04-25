a6292ce7b28597faa017485d76940d845e0532ab
Lucas Lorensetti <lucaslorensetti@hotmail.com>
2021-04-25 02:48:19 -0300

    Added INotifyPropertyChanged source generator using Source Generators of Roslyn

---
+++ GeneratedDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeneratedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("\n\nRunning AutoNotify:\n");
+            UseAutoNotifyGenerator.Run();
+
+        }
+    }
+}
